6456b636aefa6e234a0461def5c3b5337b474c0b
Bernd Ullmann <info@berndullmann.de>
2024-05-07 19:16:27 +0200

    navigation + default pages

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+import { FiPhone } from "react-icons/fi";
+import { FaRegEnvelope } from "react-icons/fa6";
 
 const Navigation = () => {
   const currentPath = usePathname();
@@ -36,8 +38,6 @@ const Navigation = () => {
       label: "Kontakt",
       href: "/kontakt",
     },
-
-    ,
   ];
   return (
     <>
@@ -80,20 +80,15 @@ const Navigation = () => {
                     )
                 )}
             </ul>
-            <a
-              href="kontakt.html"
-              className="text-white text-uppercase fw-semibold"
-              style={{ fontSize: "14px" }}
-            >
+            <Link href="/kontakt" className="nav-link">
               Kostenloses Probetraining
-            </a>
+            </Link>
           </div>
         </div>
       </nav>
 
       <div
         className="offcanvas offcanvas-start"
-        // tabindex="-1"
         id="offcanvasExample"
         aria-labelledby="offcanvasExampleLabel"
       >
@@ -119,135 +114,39 @@ const Navigation = () => {
         <div className="offcanvas-body">
           <div className="">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a
-                  className="nav-link text-primary"
-                  id="home-link"
-                  href="index.html"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link text-primary"
-                  id="wingtzun"
-                  href="wingtzun.html"
-                >
-                  Wing Tzun
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link text-primary"
-                  id="training"
-                  href="training.html"
-                >
-                  Angebot
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link text-primary"
-                  id="preise"
-                  href="preise.html"
-                >
-                  Preise
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link text-primary"
-                  id="schule"
-                  href="schule.html"
-                >
-                  Das Team
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link text-primary"
-                  id="termine"
-                  href="termine.html"
-                >
-                  News
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link text-primary"
-                  id="kontakt"
-                  href="kontakt.html"
-                >
-                  Kontakt
-                </a>
-              </li>
+              {links &&
+                links.map(
+                  (link) =>
+                    link && (
+                      <li className="nav-item" key={link.href}>
+                        <Link
+                          className="nav-link text-primary"
+                          key={link.href}
+                          href={link.href}
+                        >
+                          {link.label}
+                        </Link>
+                      </li>
+                    )
+                )}
             </ul>
             <hr />
             <p className="fs-6 mb-3 d-flex align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                style={{
-                  height: "20px",
-                  width: "auto",
-                  display: "inline-block",
-                  marginRight: "5px",
-                }}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                />
-              </svg>
+              <FiPhone
+                style={{ marginRight: "5px", height: "20px", width: "auto" }}
+              />
               0177 6460429
             </p>
             <p className="fs-6 mb-3 d-flex align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                style={{
-                  height: "20px",
-                  width: "auto",
-                  display: "inline-block",
-                  marginRight: "5px",
-                }}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                />
-              </svg>
+              <FiPhone
+                style={{ marginRight: "5px", height: "20px", width: "auto" }}
+              />
               01577 2035061
             </p>
             <p className="fs-6 mb-3 d-flex align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                style={{
-                  height: "20px",
-                  width: "auto",
-                  display: "inline-block",
-                  marginRight: "5px",
-                }}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-                />
-              </svg>
+              <FaRegEnvelope
+                style={{ marginRight: "5px", height: "20px", width: "auto" }}
+              />
               info(at)wingtzun-muenchen.de
             </p>
           </div>
